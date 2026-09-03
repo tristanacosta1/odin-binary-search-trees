1672e0cb26ed87776fba6e3716d3b96fed0a7555
Tristan <tristanacosta03@gmail.com>
2026-09-03 13:53:17 +0800

    feat: implement buildTree to only accept arrays

--- src/Tree.js
+++ src/Tree.js
@@ -3,6 +3,6 @@ export default class Tree {
         this.root = this.buildTree(array);
     }
     buildTree(array) {
-        if(!Array.isArray(array)) throw new Error("Must be initialized with arrays.")
-}
+        if (!Array.isArray(array)) throw new Error("Must be initialized with arrays.");
+    }
 }
--- src/Tree.test.js
+++ src/Tree.test.js
@@ -1,13 +1,12 @@
-import Tree from "./Tree.js"
+import Tree from "./Tree.js";
 
 describe("Tree Class", () => {
     describe("Initialization", () => {
-        test.each([42, "foo", { foo: "bar" }, null, undefined])(
-        "throws an error for %p",
-        (input) => {
-            expect(() => new Tree(input)).toThrow("Must be initialized with arrays.");
+        test("throws an error for non-array inputs", () => {
+            const invalidInputs = [42, "foo", { foo: "bar" }, null, undefined];
+            invalidInputs.forEach((input) => {
+                expect(() => new Tree(input)).toThrow("Must be initialized with arrays.");
+            });
         });
     });
 });
-
-
