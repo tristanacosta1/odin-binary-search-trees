85fa4215a89cd657c2ebcd4da2f87c044ece8122
Tristan <tristanacosta03@gmail.com>
2026-09-04 09:12:07 +0800

    test: verify that tree class sets root value

--- src/Tree.test.js
+++ src/Tree.test.js
@@ -1,12 +1,18 @@
 import Tree from "./Tree.js";
 
 describe("Tree Class", () => {
-    describe("Initialization", () => {
-        test("throws an error for non-array inputs", () => {
-            const invalidInputs = [42, "foo", { foo: "bar" }, null, undefined];
+    describe("constructor", () => {
+        test("throws for empty arrays and non-array values", () => {
+            const invalidInputs = [42, "foo", { foo: "bar" }, null, undefined, []];
             invalidInputs.forEach((input) => {
-                expect(() => new Tree(input)).toThrow("Must be initialized with arrays.");
+                expect(() => new Tree(input)).toThrow("Must be initialized with non-empty arrays.");
             });
+        });
+    });
+    describe("root", () => {
+        test("is set when array is valid", () => {
+            const tree = new Tree([1, 2, 3, 4, 5]);
+            expect(tree).toBeDefined();
         });
     });
 });
