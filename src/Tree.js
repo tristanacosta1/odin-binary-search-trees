e4da94fbd07614ad3e58b1ee10ba9f0c6adc000f
Tristan <tristanacosta03@gmail.com>
2026-09-04 11:20:07 +0800

    refactor: pass start and end bounds to buildTree()

--- src/Tree.js
+++ src/Tree.js
@@ -2,12 +2,12 @@ export default class Tree {
     constructor(array) {
         this.root = this.buildTree(array);
     }
-    buildTree(array) {
+    buildTree(array, start = 0, end) {
         if (!Array.isArray(array) || array.length === 0)
             throw new Error("Must be initialized with non-empty arrays.");
-
         const mid = Math.floor((0 + array.length - 1) / 2);
         const root = array[mid];
+        end = array.length - 1;
 
         return root;
     }
--- src/Tree.test.js
+++ src/Tree.test.js
@@ -11,12 +11,15 @@ describe("Tree Class", () => {
     });
     describe("root", () => {
         test("is set when array is valid", () => {
-            const tree = new Tree([1, 2, 3, 4, 5]);
+            const tree = new Tree([1, 2, 3]);
             expect(tree.root).toBeDefined();
         });
     });
     test("sets the middle element of the array as the root", () => {
-        const tree = new Tree([1, 2, 3, 4, 5]);
+        const tree = new Tree([1, 2, 3]);
         expect(tree.root).toBe(3);
     });
+    // Test if nodes are properly set
+    // Test with arrays with duplicate values
+    // Test with unsorted arrays
 });
